065d6df2552c713cb82105c0db081e3805261f90
NicolasAyal <javier17utn@gmail.com>
2023-12-07 22:01:38 -0300

    produto data

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -1,18 +1,38 @@
 import '../scss/description.scss'
 
 const Description = () => {
+
+  const Product =
+    {
+      company:'sneaker company',
+      title:'Fall Limited Edition Sneakers',
+      description:'these low-profile sneakers are your perfect casual wear companion. Feacturing a durable rubber outer sole, they´ll withstand everything the weather can offer.',
+      price:250.00,
+      discount:0.5,
+    }
+  
+
   return (
     <>
       <div className="contentDescription">
         <span className="descriptionSpan">
-          sneaker company
+          {Product.company}
         </span>
         <h1>
-          Fall Limited Edition Sneakers
+          {Product.title}
         </h1>
         <p>
-          these low-profile sneakers are your perfect casual wear companion. Feacturing a durable rubber outer sole, they´ll withstand everything the weather can offer.
+          {Product.description}
         </p>
+        <section className="priceProducto">
+          <div className="Price">
+            <h3>$ {(Product.price*Product.discount).toFixed(2)}</h3>
+            <h4>${(Product.price).toFixed(2)}</h4>
+          </div>
+          <div className='discount'>
+              {Product.discount*100} %
+          </div>
+        </section>
       </div>
     </>
   )
